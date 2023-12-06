6e2cd7607efa41f7a45bc0de33b2e82f4ef9eed9
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-06 11:15:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Enter ur first num ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter ur sec num ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("num1 divided by num2 = ");
+                Console.WriteLine(num1 / num2);
+
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("thats not a digit");
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
